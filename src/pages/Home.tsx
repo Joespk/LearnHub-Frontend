9f757d2316e94203023857f5baa55f78edd4b140
Joespk <supakritjoe@gmail.com>
2023-10-09 16:41:40 +0700

    Create button create new content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,28 @@ import Tap from '../componets/Tap'
 import classes from '../pages/Home.module.css'
 import useContents from '../hook/useContents'
 import Content from '../componets/Content'
+import { useAuth } from '../provider/AuthProvider'
+import { Link } from 'react-router-dom'
 
 function Home() {
   const { contents } = useContents()
-
+  const { isLoggedIn, logout } = useAuth()
   return (
     <div className={classes.container}>
       <Tap />
+      {isLoggedIn ? (
+        <>
+          <Link to="/" className={classes.cretecontentLogin}>
+            Create new content
+          </Link>
+        </>
+      ) : (
+        <>
+          <Link to="/" className={classes.cretecontentLogout}>
+            Create new content
+          </Link>
+        </>
+      )}
       <div className={classes.feedContainer}>
         {contents &&
           contents.data.map((content) => {
