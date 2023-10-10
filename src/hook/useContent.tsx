c5277bb7159e8b739c3a34c812c8bfeea62284d8
Joespk <supakritjoe@gmail.com>
2023-10-10 16:53:16 +0700

    editdetail

--- src/hook/useContent.tsx
+++ src/hook/useContent.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react'
-import { ContentDTO } from '../types/dto'
+import { ContentDTO, UpdateContentDTO } from '../types/dto'
 import axios from 'axios'
 
 const useContent = (id: string) => {
   const [content, setContentDetail] = useState<ContentDTO | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string>('')
-
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const [updateContent, setUpdate] = useState<UpdateContentDTO | null>(null)
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true)
@@ -24,7 +25,30 @@ const useContent = (id: string) => {
     fetchData()
   }, [id])
 
-  return { content, isLoading, error }
+  const newupdateContent = async (Newcomment: string, Newrating: number) => {
+    const token = localStorage.getItem('token')
+    const newContentBody: UpdateContentDTO = {
+      comment: Newcomment,
+      rating: Newrating,
+    }
+
+    setIsSubmitting(true)
+    try {
+      const res = await axios.patch<UpdateContentDTO>(
+        `https://api.learnhub.thanayut.in.th/content/${id}`,
+        newContentBody,
+        {
+          headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+        },
+      )
+      setUpdate(res.data)
+    } catch (err) {
+      throw err
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+  return { content, isLoading, error, newupdateContent, isSubmitting }
 }
 
 export default useContent
