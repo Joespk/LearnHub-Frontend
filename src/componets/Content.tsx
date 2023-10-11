5d702b0cee8e95a15c730cd0350f053ccb05272c
Joespk <supakritjoe@gmail.com>
2023-10-11 17:28:42 +0700

    hearticon

--- src/componets/Content.tsx
+++ src/componets/Content.tsx
@@ -1,11 +1,23 @@
 import { Link } from 'react-router-dom'
 import { ContentDTO } from '../types/dto'
 import classes from './Content.module.css'
-import useContent from '../hook/useContent'
+import Rating from '@mui/material/Rating'
+import { Typography, styled } from '@mui/material'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 
 interface IContentprops {
   content: ContentDTO
 }
+
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#ff6d75',
+  },
+  '& .MuiRating-iconHover': {
+    color: '#ff3d47',
+  },
+})
 
 const Content = ({ content }: IContentprops) => {
   return (
@@ -16,7 +28,14 @@ const Content = ({ content }: IContentprops) => {
         <p>{content.creatorName}</p>
         <p> {content.comment}</p>
         <p>{content.postedBy.name}</p>
-        <p>{content.rating}</p>
+        <Typography component="legend"></Typography>
+        <StyledRating
+          name="read-only"
+          value={content.rating}
+          readOnly
+          icon={<FavoriteIcon fontSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
       </Link>
     </div>
   )
--- src/pages/EditDetail.tsx
+++ src/pages/EditDetail.tsx
@@ -2,7 +2,21 @@ import { useNavigate, useParams } from 'react-router-dom'
 import useContent from '../hook/useContent'
 import { FormEvent, useState } from 'react'
 import classes from './Home.module.css'
+import { Rating, Typography, styled } from '@mui/material'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 
+//icon rating
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#ff6d75',
+  },
+  '& .MuiRating-iconHover': {
+    color: '#ff3d47',
+  },
+})
+
+// Edit Feature
 const EditDetail = () => {
   const { id } = useParams()
   const { isSubmitting, newupdateContent } = useContent(id || '1')
@@ -31,7 +45,14 @@ const EditDetail = () => {
         <label>Comment</label>
         <input type="text" value={Newcomment} onChange={(e) => setNewcomment(e.target.value)} required />
         <label>Rating</label>
-        <input type="number" value={Newrating} onChange={(e) => setNewrating(e.target.valueAsNumber)} required />
+        <Typography component="legend"></Typography>
+        <StyledRating
+          name="simple-controlled"
+          value={Newrating}
+          onChange={(e, rating) => setNewrating(rating !== null ? rating : 0)}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+        />
         <button type="submit" disabled={isSubmitting}>
           {isSubmitting ? 'Submitting...' : 'Submit'}
         </button>
