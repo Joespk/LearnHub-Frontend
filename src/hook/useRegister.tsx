dd583f57c66b929de03a3991c1730a08ae41cc7c
Joespk <supakritjoe@gmail.com>
2023-10-11 14:55:52 +0700

    register

---
+++ src/hook/useRegister.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { RegisterDTO } from '../types/dto'
+
+const useRegister = () => {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const register = async (Newusername: string, Newpassword: string, Newname: string) => {
+    const newUserBody: RegisterDTO = {
+      username: Newusername,
+      password: Newpassword,
+      name: Newname,
+    }
+    setIsSubmitting(true)
+    try {
+      const res = await axios.post<RegisterDTO>('https://api.learnhub.thanayut.in.th/user', newUserBody, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+      console.log(res.data)
+    } catch (err) {
+      throw err
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  return { isSubmitting, register }
+}
+
+export default useRegister
