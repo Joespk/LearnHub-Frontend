aa85e09ce6f8c2657312a3a2d05f1f19e6f2c68f
Joespk <supakritjoe@gmail.com>
2024-01-05 23:08:09 +0700

    add ui tap

--- src/componets/Tap.tsx
+++ src/componets/Tap.tsx
@@ -1,12 +1,22 @@
 import classes from './Tap.module.css'
 import learnhub from '../assets/content-video_1-removebg-preview.png'
+import rectangle from '../assets/Rectangle.svg'
 
 const Tap = () => {
   return (
     <section className={classes.container}>
       <div className={classes.tap}>
         <div className={classes.title}>
-          <span>Welcome to LearnHub</span>
+          <span>
+            <img src={rectangle} />
+            &nbsp; Welcome to LearnHub
+          </span>
+          <h1>The best video content website.</h1>
+          <span className={classes.mind}>
+            Expand Your Mind, Watch Your World: Learn Hub <br />
+            Where Knowledge Meets Vision. Empower Your Journey, <br /> Illuminate Your Path: Learn Hub Unveiling Wisdom,
+            Framing Futures.
+          </span>
         </div>
         <img src={learnhub} />
       </div>
